3a68f91a1fd940d2a8a65708c86ec905e8c1cc72
Eric Gallager <egallager@comcast.net>
2012-07-26 16:49:19 -0400

    Fix problems, it works now!

--- Adder_main.c
+++ Adder_main.c
@@ -41,20 +41,20 @@
  * This function is pass-by-reference, so you will need an array in main that will take the results of this array.
  * (This function was largely provided by the teaching assistant)
  */
-void GetNumber(char Input[], int Number[]); // Function prototype
-void
+int GetNumber(char Input[], int Number[]); // Function prototype
+int
 GetNumber(char Input[], int Number[]) // was originally "void GetNumber(char* Input,int Number[])" in TA's code
 {
     int i = 0; // Initialize loop-counter for for-loop
-	printf("\n i = %i \n", i); // Statement for debugging
+	// printf("\n i = %i \n", i); // Statement for debugging
 	int j = 0; // Initialize loop-counter for second while-loop
-	printf("\n j = %i \n", j); // Statement for debugging
+	// printf("\n j = %i \n", j); // Statement for debugging
     int Temp = 0; // Initialize number used to store numbers in in second while-loop
-	int length = LENGTH; // Convert from constant to variable
-	printf("\n length is %i \n", length); // Statement for debugging
-	printf("\n Number[i] is %i \n", Number[i]); // Statement for debugging (another place EXC_BAD_ACCESS errors can appear)
+	// int length = LENGTH; // Convert from constant to variable
+	// printf("\n length is %i \n", length); // Statement for debugging
+	// printf("\n Number[i] is %i \n", Number[i]); // Statement for debugging (another place EXC_BAD_ACCESS errors can appear)
 	Number[i] = 0; // Re-initialize, just in case
-	printf("\n Number[i] is %i \n", Number[i]); // Statement for debugging
+	// printf("\n Number[i] is %i \n", Number[i]); // Statement for debugging
 
 	/* This following for-loop sets all of the elements of Number[] to zero.
 	 * LENGTH was a define in my code; you can have lengths >= 100.
@@ -62,15 +62,13 @@ GetNumber(char Input[], int Number[]) // was originally "void GetNumber(char* In
     for(i = 0; i < LENGTH; i++)
     {
         Number[i] = 0; // This can cause an EXC_BAD_ACCESS error, add a breakpoint (error seemed to disappear here (at first) when compiled without optimization, i.e. by using the -O0 compiler flag)
-		printf("\n Number[%i] is %i \n", i, Number[i]); // Statement for debugging
+		// printf("\n Number[%i] is %i \n", i, Number[i]); // Statement for debugging
     }
-	printf("\n i = %i \n", i); // Statement for debugging
+	// printf("\n i = %i \n", i); // Statement for debugging
     i = 0; // originally was i = 0 in the teaching assistant's code, I changed to 1 to see if that would fix Input[i], then changed it back after that did nothing
-	printf("\n Number[i] is %i \n", Number[i]); // Statement for debugging
-	printf("\n i = %i \n", i); // Statement for debugging
-	printf("\n Initializing Number array to 0 done. \n"); // Statement for debugging
-	Input[i] = 0; // This was originally put in to re-initialize Input[i] to try to stop the error from below from occurring, unfortunately it just causes the error to happen here now instead
-	printf("\n Input[i] = %c", Input[i]); // Statement for debugging
+	// printf("\n Number[i] is %i \n", Number[i]); // Statement for debugging
+	// printf("\n i = %i \n", i); // Statement for debugging
+	// printf("\n Initializing Number array to 0 done. \n"); // Statement for debugging
 
     /* This while-loop grabs every element in Input and places them in Number[] after subtracting 48
      * We're assuming that these are ASCII characters, which will give us the actual numbers after subtracting 48
@@ -79,13 +77,16 @@ GetNumber(char Input[], int Number[]) // was originally "void GetNumber(char* In
     while(Input[i]!='\0') // The previous EXC_BAD_ACCESS error crops back up here though, even when compiled unoptimized.
     {
         Number[i] = (int)Input[i]-48;
+		// printf("\n Number[%i] = %i \n", i, Number[i]); // Statement for debugging
         i++;
     }
     j = 0; // Reset it, just in case
-	printf("\n j = %i \n", j); // Statement for debugging
+	int arglen = i; // Take whatever the loop counter got up to and store it
+	// printf("\n arglen = %i \n", i); // Statement for debugging
+	// printf("\n j = %i \n", j); // Statement for debugging
     i--; // I don't really know why this is here, but it was in the original code
-	printf("\n i = %i \n", i); // Statement for debugging
-	printf("\n Done entering input. \n"); // Statement for debugging
+	// printf("\n i = %i \n", i); // Statement for debugging
+	// printf("\n Done entering input. \n"); // Statement for debugging
 
     /* The endianness of the numbers will be wrong right after inputing them.
 	 * This while-loop simply does some flipping.
@@ -95,10 +96,12 @@ GetNumber(char Input[], int Number[]) // was originally "void GetNumber(char* In
         Temp=Number[i];
         Number[i]=Number[j];
         Number[j]=Temp;
+		// printf("\n Number[%i] is %i \n", i, Number[i]); // Statement for debugging
         j++;
         i--;
     }
-	printf("\n Done flipping endianness. \n"); // Statement for debugging
+	// printf("\n Done flipping endianness. \n"); // Statement for debugging
+	return arglen;
 }
 
 /*
@@ -106,47 +109,62 @@ GetNumber(char Input[], int Number[]) // was originally "void GetNumber(char* In
  */
 int main(int argc, const char * argv[]) { // Convention for declaring main() from the C specification
 	if (argc != 3) { // check to make sure number of arguments is correct
-		printf("\n Incorrect number of arguments specified! \n"); // just in case
+		printf("\n Incorrect number of arguments specified! This program can only add 2 numbers. \n"); // just in case
+		printf(" Please try again. \n");
+		exit(0); // end program
 	} else {
-		printf("\n argc is %i \n", argc); // Statement for debugging (should be 3)
+		printf("\n You are adding _%i_ numbers. \n", argc-1);
+		// printf("\n argc is %i \n", argc); // Statement for debugging (should be 3)
 	}
-	printf("\n This printf statement uses the placeholder \"%%s\": %s \n", "\"string\""); // Testing using %s to print strings
-	printf("\n First character of argv[0] is %c \n", *argv[0]); // Statement for debugging (Warning: %c can only display one character at a time. I'd use %s but that causes errors.)
-	printf("\n First character of argv[1] is %c \n", *argv[1]); // Statement for debugging (Warning: %c can only display one character at a time. I'd use %s but that causes errors.)
-	printf("\n First character of argv[2] is %c \n", *argv[2]); // Statement for debugging (Warning: %c can only display one character at a time. I'd use %s but that causes errors.)
-	int *MyFirstNumber[LENGTH]; // Moved out of function paratmater list per advice found online, it should be an output array
+	// printf("\n argv[0] is %s \n", argv[0]); // Statement for debugging
+	// printf("\n argv[1] is %s \n", argv[1]); // Statement for debugging
+	// printf("\n argv[2] is %s \n", argv[2]); // Statement for debugging
+	int MyFirstNumber[LENGTH]; // Moved out of function paratmater list per advice found online, it should be an output array
 	int j = 0; // Initialize loop-counter
-	printf("\n j is %i \n", j); // Statement for debugging
+	// printf("\n j is %i \n", j); // Statement for debugging
 	for (j = 0; j < LENGTH; j++) // goes through each element of MyFirstNumber[]
 	{
 		MyFirstNumber[j] = 0; // sets each array element in MyFirstNumber[] to 0
-		printf("\n MyFirstNumber[%i] is %i \n", j, MyFirstNumber[j]); // Statement for debugging (this might give a compiler warning, but that's better than a runtime error for a simple print function)
+		// printf("\n MyFirstNumber[%i] is %i \n", j, MyFirstNumber[j]); // Statement for debugging
 	}
-	j = 0; // Reset j
-	printf("\n MyFirstNumber[j] is %i \n", MyFirstNumber[j]); // Statement for debugging (this might give a compiler warning, but that's better than a runtime error for a simple print function)
-    GetNumber((char*)argv[1], /*(int*)*/MyFirstNumber[j]); // Originally this was copied from assignment, I've been messing with it though
-    printf("\n MyFirstNumber[j] is %i \n", MyFirstNumber[j]); // Statement for debugging (clang's static analyzer said at one point that this is a dereference of a null pointer, but I haven't managed to get my program to run this far in the first place, so it's a moot point)
-	int *MySecondNumber[LENGTH]; // output array for second number
+	// j = 0; // Reset j
+	// printf("\n MyFirstNumber[j] is %i \n", MyFirstNumber[j]); // Statement for debugging
+    int arglen1 = GetNumber((char*)argv[1], (int*)MyFirstNumber); // Originally this was copied from assignment, I've been messing with it though
+    // printf("\n arglen1 = %i \n", arglen1); // Statement for debugging
+	// printf("\n MyFirstNumber[j] is %i \n", MyFirstNumber[j]); // Statement for debugging
+	int MySecondNumber[LENGTH]; // output array for second number
 	for (j = 0; j < LENGTH; j++) // goes through each element of MySecondNumber[]
 	{
 		MySecondNumber[j] = 0; // sets each array element in MySecondNumber[] to 0
-		printf("\n MySecondNumber[%i] is %i \n", j, MySecondNumber[j]); // Statement for debugging (this might give a compiler warning, but that's better than a runtime error for a simple print function)
+		// printf("\n MySecondNumber[%i] is %i \n", j, MySecondNumber[j]); // Statement for debugging
 	}
-	j = 0; // Reset j
-	GetNumber((char*)argv[2], /*(int*)*/MySecondNumber[j]); // Repeat what we just did with the first number with our second
-	printf("\n MySecondNumber[j] is %i \n", MySecondNumber[j]); // Statement for debugging
+	// j = 0; // Reset j
+	int arglen2 = GetNumber((char*)argv[2], (int*)MySecondNumber); // Repeat what we just did with the first number with our second
+	// printf("\n arglen2 = %i \n", arglen2); // Statement for debugging
+	// printf("\n MySecondNumber[j] is %i \n", MySecondNumber[j]); // Statement for debugging
+	printf("\n %s + %s = ", argv[1], argv[2]);
 	int Answer[LENGTH];
 	for (j = 0; j < LENGTH; j++) // Go through entire Answer array
 	{
-		Answer[j] = (*MyFirstNumber[j] + *MySecondNumber[j]); // Add arrays together
+		Answer[j] = (MyFirstNumber[j] + MySecondNumber[j] + Answer[j]); // Add arrays together (Adding it to itself is just in case there's already something into it due to place value)
+		if (Answer[j] >= 10) { // handle place-value
+			Answer[j] = Answer[j] - 10; // You only ever carry a 1 when adding 2 numbers
+			Answer[j+1]++; // Put into next digit
+		}
 	}
-	while(Answer[j] != '\0') // Go through Answer[] array until getting to the end of the result
+	j--; // Put j back down to the array size
+	while (Answer[j] == 0) // Skip past all the 0s (with j-- below)
 	{
-		printf("%i", Answer[j]); // No spaces so digits will be next to each other
-		j++;
+		j--;
 	}
-	j = 0; // Reset j
-	printf("\n j is %i \n", j); // Statement for debugging
+	while (j >= 0) // Go through array backwards to print it (with j-- below)
+	{
+		printf("%i", Answer[j]);
+		j--;
+	}
+	// j = 0; // Reset j
+	// printf("\n j is %i \n", j); // Statement for debugging
+	printf(" \n "); // I like putting extra whitespace in
     return 0;
 }
 
